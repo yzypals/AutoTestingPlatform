83364b791e6e9c58a3f98f0b7f3ed3cc097a957b
授客 <laifyu@sf-express.com>
2019-02-20 15:59:55 +0800

    修复 文件：说明/AutotestPlatform/settings.py代码配置错误

--- AutotestPlatform/说明/AutotestPlatform/settings.py
+++ AutotestPlatform/说明/AutotestPlatform/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*','127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'website.apps.WebsitConfig',
+    'website.apps.WebsiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
